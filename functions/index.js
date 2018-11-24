cd49f3d562e55a2ad9119e6c7b1b8a6c9fe738c0
shinzenkoru <empongpat@gmail.com>
2018-11-24 19:24:45 +0700

    Initialize Firebase Cloud Functions

---
+++ functions/index.js
@@ -0,0 +1,7 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+exports.LineBot = functions.https.onRequest((request, response) => {
+  response.send("Hello World!");
+});
